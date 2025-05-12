0e233b42a09841ba22978b300b54fe9adba9507b
Kkyfd <l20f21@outlook.com.br>
2025-05-12 13:19:15 -0300

    Outro teste

--- cypress/e2e/footer.cy.js
+++ cypress/e2e/footer.cy.js
@@ -1,7 +1,20 @@
 describe('Footer Tests', () => {
     it('Deve exibir copyright', () => {
-      cy.visit('http://localhost:8080/src/pages/');
+      cy.visit('/src/pages/index.html');
       cy.get('.footer').should('contain', '©');
+      cy.wait(500);
+
+      cy.visit('/src/pages/biografia.html');
+      cy.get('.footer').should('contain', '©');
+      cy.wait(500);
+
+      cy.visit('/src/pages/projetos.html');
+      cy.get('.footer').should('contain', '©');
+      cy.wait(500);
+
+      cy.visit('/src/pages/contato.html');
+      cy.get('.footer').should('contain', '©');
+      cy.wait(500);
     });
   });
   
