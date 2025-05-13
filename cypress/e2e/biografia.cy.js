4f124f716c9f7f985bf40d24c35f49f9bdcfcb72
KkyFd <KkyFd@users.noreply.github.com>
2025-05-13 04:22:47 +0000

    deploy: f34334f01ba8cd3c69f287020355d5b4501dde57

--- cypress/e2e/biografia.cy.js
+++ cypress/e2e/biografia.cy.js
@@ -7,7 +7,7 @@ describe('Biografia Page Tests', () => {
     it('Deve haver imagens no carrossel e navegar corretamente', () => {
       cy.visit('/src/pages/biografia.html');
       
-      cy.get('.carousel img').should('have.length.at.least', 3);
+      cy.get('.carousel img').should('have.length.at.least', 4);
       
       cy.get('.carousel-image1').should('exist').and('be.visible');
 
@@ -17,6 +17,14 @@ describe('Biografia Page Tests', () => {
 
       cy.get('.next').click();
       cy.get('.carousel-image2').should('not.be.visible');
+      cy.get('.carousel-image3').should('be.visible');
+
+      cy.get('.next').click();
+      cy.get('.carousel-image3').should('not.be.visible');
+      cy.get('.carousel-image4').should('be.visible');
+
+      cy.get('.prev').click();
+      cy.get('.carousel-image4').should('not.be.visible');
       cy.get('.carousel-image3').should('be.visible');
 
       cy.get('.prev').click();
